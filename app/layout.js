728d2cce1a079d7b2556c39e9b85ef69c9679bfd
Ankit Kaushal <ankitkaushal288@gmail.com>
2025-03-31 11:42:11 +0530

    site verification

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,10 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<meta name="google-adsense-account" content="ca-pub-1816177424340336" />
+			<meta
+				name="google-site-verification"
+				content="ZdTxiXrK0yjpYGk5ggg9hsAuDwAsffJ4NVTFA-3CMys"
+			/>
 			<script
 				async
 				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1816177424340336"
